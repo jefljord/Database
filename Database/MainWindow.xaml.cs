fdfdd26134a490fc48c2ac5cc2d8655bd65c5d09
jeff jordan <jeffjordan33@outlook.com>
2019-09-24 11:42:17 -0500

    dkd

--- Database/MainWindow.xaml.cs
+++ Database/MainWindow.xaml.cs
@@ -22,15 +22,58 @@ namespace Database
     public partial class MainWindow : Window
     {
         OleDbConnection cn;
+        string data = "";
+        string data2 = "";
+        string data3 = "";
         public MainWindow()
         {
             InitializeComponent();
-            cn = new OleDbConnection( @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source =| DataDirectory |\WorkPlace11.accdb" );
+            cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\WorkPlace11.accdb");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+            string query = "select* from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            
+            while (read.Read()){
+                data += read[0].ToString() + "\n";
+                data2 += read[1].ToString() + "\n";
+                data3 += read[2].ToString() + "\n";
+            }
+
+            DataBox.Text = data + data2;
+            cn.Close();
            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) // Didn't remove to avoid build issues.
+        {
+            
+        
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            string query = "select* from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+
+            while (read.Read())
+            {
+                data += read[0].ToString() + "\n";
+                data2 += read[1].ToString() + "\n";
+                data3 += read[2].ToString() + "\n";
+            }
+
+            DataBox.Text = data + data2;
+            cn.Close();
+
         }
     }
 }
